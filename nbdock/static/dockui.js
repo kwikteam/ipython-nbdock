2edf6a806d5d57f89ad807976302d2f78d90ff2b
Cedric GESTES <gestes@aldebaran-robotics.com>
2015-02-05 16:49:49 +0100

    cell: add a toolbar to support docking/undocking

--- nbdock/static/dockui.js
+++ nbdock/static/dockui.js
@@ -222,6 +222,46 @@ define(function (require) {
 
     // load_css('/nbextensions/testing/exercise/main.css');
 
+
+
+    // Create and register the method that creates the checkbox that
+    // alters the new 'exclude'
+    // flag of the cell's metadata.
+    var flag_name = 'docked';
+    var cell_flag_init = IPython.CellToolbar.utils.checkbox_ui_generator(
+
+        // Name
+        flag_name,
+
+        // Setter, called when the checkboxes state has been changed.
+        // Sets the flag name and
+        // value in the cell's metadata.
+        function(cell, value){
+            cell.metadata[flag_name] = value;
+        },
+
+        // Getter, called when the control is rendered.  Determines the initial
+    // state of the checkbox.  If the flag doesn't in the metadata, default
+        // to false.
+        function(cell){
+            if (cell.metadata[flag_name] === undefined || cell.metadata[flag_name]
+                == false) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    );
+    IPython.CellToolbar.register_callback(flag_name, cell_flag_init);
+
+    // Create and register the toolbar with IPython.
+    IPython.CellToolbar.register_preset('Export Cell Exclusion', [flag_name]);
+
+
+    events.on('create.Cell', function() {
+        console.log("hacking cell:");//, index, cell);
+    });
+
     setupDockUi();
     createWindows();
 
